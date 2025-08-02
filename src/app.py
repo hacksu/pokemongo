7156dc4c3bbdaff3c01987de2fb94a0b1bf4d790
Ender <nallahabi2003@gmail.com>
2025-08-02 15:22:35 -0400

    Implemented pokemon themed flask app for nice visuals

--- src/app.py
+++ src/app.py
@@ -1,15 +1,35 @@
-from flask import Flask, render_template
-import game
+from flask import Flask, render_template, request, redirect, jsonify
+import base64
+from game import get_pokedex, get_pc, battle, battle_resolve, get_pokemon_from_name
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template("index.html", pokemon=get_pokedex(), pc=get_pc())
 
-@app.route('/pokedex')
-def pokedex():
-    return render_template("pokedex.html", pokemon=get_pokedex())
+@app.route('/battle', methods=['POST'])
+def fight():
+    pokemon_name = request.form.get('pokemon_name')
+    if pokemon_name:
+        result = battle(pokemon_name)
+        if 'msg' in result and result['msg'] == "Error Occurred!":
+            return redirect('/?tab=battle')
+        else:
+            return render_template("battle.html",
+                                   red_pokemon=get_pokemon_from_name(pokemon_name),
+                                   blue_pokemon=get_pokemon_from_name(result['enemy']))
+        
+@app.route('/results', methods=['POST'])
+def resolve_fight():
+    choice = request.form.get('choice')
+    red_pokemon = get_pokemon_from_name(request.form.get('red_pokemon'))
+    blue_pokemon = get_pokemon_from_name(request.form.get('blue_pokemon'))
+
+    results = battle_resolve(choice=choice, red_pokemon=red_pokemon, blue_pokemon=blue_pokemon)
+    enc_results = base64.b64encode(results["msg"].encode()).decode()
+    return redirect(f'/?tab=battle&results={enc_results}')
 
 if __name__ == "__main__":
-    app.run(debug=False)
+    # listen on all interfaces
+    app.run(host='0.0.0.0', debug=False)
--- src/game.py
+++ src/game.py
@@ -1,7 +1,10 @@
 from pymongo import MongoClient
 from random import randrange
 
-client = MongoClient("mongodb://localhost:27017")
+# mongodb credential
+cred="admin:password123"
+
+client = MongoClient(f"mongodb://{cred}@localhost:27017")
 db = client.pokemon
 
 pc_collection = db.pc
@@ -12,7 +15,61 @@ def get_pokedex():
     pokedex_data = list(pokedex_collection.find({}))
     return pokedex_data
 
-# returns the pc collection as a list
+# returns the entire pc collection as a list
 def get_pc():
     pc_data = list(pc_collection.find({}))
     return pc_data
+
+def get_pokemon_from_name(name):
+    result = list(pc_collection.find({"name":name}))
+    if len(result) == 1:
+        return result[0]
+    else:
+        return None
+
+def battle(name):
+    result = list(pc_collection.find({"name":name}))
+    my_pokemon = None
+    if len(result) == 1:
+        my_pokemon = result[0]
+    else:
+        return { "msg":"Error Occurred!" }
+
+    available_pokemon = pc_collection.count_documents({})
+
+    random_pokemon = my_pokemon
+    # ensure random pokemon we are going to fight
+    # is not the pokemon we are using
+    while random_pokemon == my_pokemon:
+        random_index = randrange(0, available_pokemon)
+        random_pokemon = list(pc_collection.find({}))[random_index]
+        
+    if "xp" in random_pokemon and "hp" in random_pokemon:
+        return { "enemy":random_pokemon["name"] }
+    else:
+        return { "msg":"Error Occurred!" }
+
+def battle_resolve(choice:str, red_pokemon, blue_pokemon):
+    if choice.lower() == "fight":
+        hp_decrease = -randrange(0, 5)
+        xp_increase = randrange(5, 10)
+
+        # your pokemon lost health and gained XP
+        pc_collection.update_one(
+            { "_id": red_pokemon["_id"] },
+            {"$inc": {"hp": hp_decrease}}
+        )
+        pc_collection.update_one(
+            { "_id": red_pokemon["_id"] },
+            {"$inc": {"xp": xp_increase}}
+        )
+
+        return { "msg":"You Won!" }
+    else:
+        # opponent gains XP
+        pc_collection.update_one(
+            {"_id": blue_pokemon["_id"]},
+            {"$inc": {"xp": randrange(5, 10)}}
+        )
+        
+        return { "msg":"You Fled!" }
